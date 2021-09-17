b334d7827736202119f2d17bb0c46afca7f6dffc
Pavel Orlovsky <paul.fofanov@gmail.com>
2021-09-17 04:26:15 +0300

    6th commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div>
-                <a href="https://www.youtube.com/watch?v=00ZNuBIE-pM&ab_channel=IT-KAMASUTRA" className={s.item}>Profile</a>
+            <div className={s.item}>
+                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
             </div>
-            <div>
-                <a href="https://www.youtube.com/watch?v=00ZNuBIE-pM&ab_channel=IT-KAMASUTRA" className={`${s.item} ${s.active}`}>Messages</a>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
             </div>
-            <div>
-                <a href="https://www.youtube.com/watch?v=00ZNuBIE-pM&ab_channel=IT-KAMASUTRA" className={s.item}>News</a>
+            <div className={s.item}>
+                <NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
             </div>
-            <div>
-                <a href="https://www.youtube.com/watch?v=00ZNuBIE-pM&ab_channel=IT-KAMASUTRA" className={s.item}>Music</a>
+            <div className={s.item}>
+                <NavLink to="/music" activeClassName={s.activeLink}>Music</NavLink>
             </div>
-            <div>
-                <a href="https://www.youtube.com/watch?v=00ZNuBIE-pM&ab_channel=IT-KAMASUTRA" className={s.item}>Settings</a>
+            <div className={s.item}>
+                <NavLink to="settings" activeClassName={s.activeLink}>Settings</NavLink>
             </div>
         </nav>
     );
