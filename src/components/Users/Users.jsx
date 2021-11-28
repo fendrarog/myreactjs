57636fe0e07d4f40983abd8f6eb632717bedb25f
Pavel Orlovsky <paul.fofanov@gmail.com>
2021-11-28 23:53:10 +0300

    21st commit (Users API)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,66 +1,72 @@
+import * as axios from "axios";
 import React from "react";
 import s from "./Users.module.css";
-
+import userPhoto from "../../assets/images/nophoto.jpg";
 
 let Users = (props) => {
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
+  }
+  /*  {
+        id: 1,
+        photoUrl:
+          "https://www.meme-arsenal.com/memes/6c7be166c159f171e0709a1d6e57c967.jpg",
+        followed: false,
+        fullName: "Pavlik",
+        status: "I am the durik",
+        location: { cityName: "Moscow", countryName: "Russia" },
+      } */
 
-    if (props.users.length === 0) {
-    props.setUsers([
-        {
-          id: 1,
-          photoUrl:
-            "https://www.meme-arsenal.com/memes/6c7be166c159f171e0709a1d6e57c967.jpg",
-          followed: false,
-          fullName: "Pavlik",
-          status: "I am the durik",
-          location: { cityName: "Moscow", countryName: "Russia" },
-        },
-        {
-          id: 2,
-          photoUrl:
-            "https://www.meme-arsenal.com/memes/6c7be166c159f171e0709a1d6e57c967.jpg",
-          followed: true,
-          fullName: "Yana",
-          status: "I am a boss",
-          location: { cityName: "Minsk", countryName: "Belarus" },
-        },
-        {
-          id: 3,
-          photoUrl:
-            "https://www.meme-arsenal.com/memes/6c7be166c159f171e0709a1d6e57c967.jpg",
-          followed: false,
-          fullName: "Dasha",
-          status: "Gonna school",
-          location: { cityName: "Kiev", countryName: "Ukraine" },
-        },
-      ])
-    }
-    return <div>
-        {
-        props.users.map(user => <div key={user.id}> 
-        <span>
+  return (
+    <div>
+      {props.users.map((user) => (
+        <div key={user.id}>
+          <span>
             <div>
-                <img src={user.photoUrl} alt="#" className={s.userPhoto} />
+              <img
+                src={user.photos.small != null ? user.photos.small : userPhoto}
+                alt="#"
+                className={s.userPhoto}
+              />
             </div>
             <div>
-                {user.followed 
-                ? <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button> 
-                : <button onClick={() => {props.follow(user.id)}}>Follow</button>}
+              {user.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(user.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(user.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
             </div>
-        </span>
-        <span>
-        <span>
-            <div>{user.fullName}</div>
-            <div>{user.status}</div>
-        </span>
-        <span>
-            <div>{user.location.cityName}</div>
-            <div>{user.location.countryName}</div>
-        </span>
-        </span>
-        </div>)
-        }
+          </span>
+          <span>
+            <span>
+              <div>{user.name}</div>
+              <div>{user.status}</div>
+            </span>
+            <span>
+              <div>{"user.location.cityName"}</div>
+              <div>{"user.location.countryName"}</div>
+            </span>
+          </span>
+        </div>
+      ))}
     </div>
-}
+  );
+};
 
 export default Users;
